59b418a9f7b4d13a3a8edb9dafd04f786e44243a
Jacson White <109887578+JacsonWhite@users.noreply.github.com>
2023-03-21 18:48:26 -0700

    Update arraychallenge.js

--- arraychallenge.js
+++ arraychallenge.js
@@ -1,25 +1,27 @@
 // Write a function that is given an array and each time the value is "food" it should console log "yummy". 
 // If "food" was not present in the array console log "I'm hungry" once.
-function alwaysHungry(arr) {
-    var foodCount = 0;
-    for(var i=0; i<arr.length;){
-        if(arr[i] == "food"){
-            console.log("yummy");
-            foodCount++;
-        }
-        i += 1;
-    }
-    if(foodCount == 0){
-        console.log("I'm Hungry")
-    }
-}
-alwaysHungry([3.14, "food", "pie", true, "food"]);
+
+// function alwaysHungry(arr) {
+//     var foodCount = 0;
+//     for(var i=0; i<arr.length;){
+//         if(arr[i] == "food"){
+//             console.log("yummy");
+//             foodCount++;
+//         }
+//         i += 1;
+//     }
+//     if(foodCount == 0){
+//         console.log("I'm Hungry")
+//     }
+// }
+// alwaysHungry([3.14, "food", "pie", true, "food"]);
 // this should console log "yummy", "yummy"
-alwaysHungry([4, 1, 5, 7, 2]);
+// alwaysHungry([4, 1, 5, 7, 2]);
 // this should console log "I'm hungry"
 
 
 // Given an array and a value cutoff, return a new array containing only the values larger than cutoff.
+
 // function highPass(arr, cutoff) {
 //     var filteredArr = [];
 //     for(var i=0; i<arr.length; i++){
@@ -34,6 +36,7 @@ alwaysHungry([4, 1, 5, 7, 2]);
 
 
 // Given an array of numbers return a count of how many of the numbers are larger than the average.
+
 // function betterThanAverage(arr) {
 //     var sum = 0;
 //     for(var i=0; i<arr.length; i++){
@@ -54,6 +57,7 @@ alwaysHungry([4, 1, 5, 7, 2]);
 
 
 // Write a function that will reverse the values an array and return them.
+
 // function reverse(arr) {
 //     var left = 0; 
 //     var right = arr.length - 1;
@@ -73,6 +77,7 @@ alwaysHungry([4, 1, 5, 7, 2]);
 // Write a function that will return an array of Fibonacci numbers up to a given length n. 
 // Fibonacci numbers are calculated by adding the last two values in the sequence together. 
 // So if the 4th value is 2 and the 5th value is 3 then the next value in the sequence is 5.
+
 // function fibonacciArray(n) {
 //     // the [0, 1] are the starting values of the array to calculate the rest from
 //     var i;
